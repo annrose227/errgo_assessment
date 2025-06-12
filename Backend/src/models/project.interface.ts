6b2c5e2fddf882d3b7a00e44a8c1e84c0390910b
Ann <annrosemk227@gmail.com>
2025-06-12 12:59:51 +0530

    zod schema for model validation

--- Backend/src/models/project.interface.ts
+++ Backend/src/models/project.interface.ts
@@ -1,8 +1,18 @@
 /**
  * BONUS: Implement zod schema for model validation
  */
+import { z } from "zod";
+
 export interface IProject {
-    id: string;
-    name: string;
-    description: string;
+  id: string;
+  name: string;
+  description: string;
 }
+
+export const ProjectSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+});
+
+export type Project = z.infer<typeof ProjectSchema>;
